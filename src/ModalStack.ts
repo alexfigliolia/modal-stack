e26528a8bab6b07e8d97487dc5cebd47bc22cbd1
Alex Figliolia <alexfigliolia@gmail.com>
2025-05-13 21:26:09 -0400

    JS doc, add a method for updating ModalToggle methods at runtime

--- src/ModalStack.ts
+++ src/ModalStack.ts
@@ -2,15 +2,34 @@ import { QuickStack } from "@figliolia/data-structures";
 import { ModalToggle } from "./ModalToggle";
 import type { Callback } from "./types";
 
+/**
+ * Modal Stack
+ *
+ * A utility for managing UI's with multiple modals,
+ * dialogs, or drawers
+ */
 export class ModalStack extends QuickStack<Callback> {
-  public override push(item: Callback) {
-    const ID = super.push(item);
+  /**
+   * Push
+   *
+   * Add's a modal's close method to the stack. Returns
+   * a unique identifier that can be passed to `ModalStack.delete()`
+   * to remove the entry manually
+   */
+  public override push(closerFN: Callback) {
+    const ID = super.push(closerFN);
     if (super.length === 1) {
       window.addEventListener("keydown", this.keydown);
     }
     return ID;
   }
 
+  /**
+   * Pop
+   *
+   * Closes the latest modal on the stack and
+   * removes its entry
+   */
   public override pop() {
     const callback = super.pop();
     callback?.();
@@ -20,10 +39,22 @@ export class ModalStack extends QuickStack<Callback> {
     return callback;
   }
 
+  /**
+   * Create
+   *
+   * A toggle creator that'll handle managing stack
+   * entries for given set of open/close functionality
+   */
   public create<T extends any[]>(opener: Callback<T>, closer: Callback) {
     return new ModalToggle<T>(opener, closer, this);
   }
 
+  /**
+   * Close All
+   *
+   * Removes each entry from the stack and closes the
+   * associated UI
+   */
   public closeAll() {
     while (this.length) {
       this.pop();
--- src/ModalToggle.ts
+++ src/ModalToggle.ts
@@ -1,6 +1,12 @@
 import type { ModalStack } from "./ModalStack";
 import type { Callback } from "./types";
 
+/**
+ * Modal Toggle
+ *
+ * An interface to simply managing a `ModalStack`
+ * entry.
+ */
 export class ModalToggle<T extends any[] = never[]> {
   public ID?: string;
   public isOpen = false;
@@ -13,12 +19,24 @@ export class ModalToggle<T extends any[] = never[]> {
     this.closer = closer;
   }
 
+  /**
+   * Open
+   *
+   * Invokes the opener function passed through the constructor
+   * and adds the toggle's entry to the stack
+   */
   public open = (...args: T) => {
     this.isOpen = true;
     this.ID = this.stack.push(this.close);
     this.opener(...args);
   };
 
+  /**
+   * Close
+   *
+   * Invokes the closer function passed through the constructor
+   * and removes the toggle's entry from the stack
+   */
   public close = () => {
     if (this.ID) {
       this.stack.delete(this.ID);
@@ -27,4 +45,16 @@ export class ModalToggle<T extends any[] = never[]> {
     this.closer();
     this.isOpen = false;
   };
+
+  /**
+   * Update
+   *
+   * If your opener/closer functions passed through the
+   * constructor are subject to change at runtime, use this
+   * method to update them
+   */
+  public update(opener: Callback<T>, closer: Callback) {
+    this.opener = opener;
+    this.closer = closer;
+  }
 }
